d6fbdbf84a536210f054cbc07dbe78bc3a514552
AMAN02SS <7000602733aman@gmail.com>
2026-08-07 23:25:26 +0530

    Add feature storage module

--- terraform/enviornments/dev/terraform.tfvars
+++ terraform/enviornments/dev/terraform.tfvars
@@ -7,3 +7,14 @@ vnet = {
   vnet_name = "vnet-dev-infra"
   address_space = ["10.0.0.0/16"]
 }
+
+storage_account = {
+  storage_account_name = "storageacc070826"
+  container_access_type = "LRS"
+  account_tier = "Standard"
+}
+
+storage_container = {
+  container_name = "Images"
+  container_access_type = "private"
+}
--- terraform/enviornments/dev/variables.tf
+++ terraform/enviornments/dev/variables.tf
@@ -11,3 +11,18 @@ variable "vnet" {
     address_space = list(string)
   }))
 }
+
+variable "storage_account" {
+  type = map(object({
+    storage_account_name = string
+    container_access_type = string
+    account_tier = string
+  }))
+}
+
+variable "storage_container" {
+  type = map(object({
+    container_name = string
+    container_access_type = string
+  }))
+}
