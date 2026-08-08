d9bf0ad47404092767805e1cd926dc1c0862d9a0
AMAN02SS <7000602733aman@gmail.com>
2026-08-09 01:25:37 +0530

    fix: terraform module variables, providers, and outputs

--- terraform/modules/keyvault/output.tf
+++ terraform/modules/keyvault/output.tf
@@ -1,3 +1,3 @@
-output "azurerm_resource_group" {
-  value = azurerm_resource_group.this.id
+output "azurerm_key_vault" {
+  value = azurerm_key_vault.this.id
 }
--- terraform/modules/resource_group/output.tf
+++ terraform/modules/resource_group/output.tf
@@ -1,3 +1,7 @@
-output "azurerm_resource_group" {
-  value = azurerm_resource_group.this.id
+output "rg_name" {
+  value = azurerm_resource_group.this.name # Replace 'this' with your resource name if different
+}
+
+output "location" {
+  value = azurerm_resource_group.this.location # Replace 'this' with your resource name if different
 }
